4c1325954735608a4d5c14a4e17d63d2f385b565
dstangel58 <dstangel@nd.edu>
2026-02-08 14:55:46 -0500

    Added description

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,17 +1,16 @@
-# import Libraries 
+# import Libraries; each provides visualization importants (pandas and dataframes, seaborn boxplots, and streamlit visualization)
 import pandas as pd 
 import streamlit as st 
 import seaborn as sns 
-import matplotlib.pyplot as plt 
 
-# import dataset 
+# import dataset; not specific to any one user; drops null values 
 df = pd.read_csv("data/penguins.csv").dropna()
 
-# App Heading 
-st.title("Penguins By Island and Species")
+# App Heading
+st.title("Penguin Biology By Island and Species")
+st.markdown("This penguins dashboard provides an in-depth look into the penguins.csv dataset using only 120 lines of code. Through this exploratory data analysis, new trends can be discovered regarding factors such as flipper length and bill length/depth (across both species and island).")
 
-# DataFrame Selection; four categorical selection choices 
-# 1. Define your options (Keep your current logic)
+# DataFrame Selection; four categorical selection choices; creates as much flexibility as possible--even possesses "All" option; 
 island_options = ['All'] + list(df["island"].unique())
 species_options = ['All'] + list(df['species'].unique())
 sex_options = ['All'] + list(df['sex'].unique())
@@ -20,7 +19,7 @@ island = st.selectbox("Select an Island", island_options)
 species = st.selectbox("Select a Species", species_options)
 sex = st.selectbox("Select a Sex", sex_options)
 year = st.selectbox('Select a Year', year_options)
-filter_df = df.copy()
+filter_df = df.copy() # uses if statement for "All" case
 if island != "All":
     filter_df = filter_df[filter_df["island"] == island]
 if species != "All":
@@ -33,7 +32,7 @@ if year != "All":
 st.write(f"Showing results for: {species}, {island}, {sex}, {year}")
 st.dataframe(filter_df)
 
-# Setting up species-based categorization system and cleaning data 
+# Setting up species-based categorization system and cleaning data; provides a dictionary that renames columns to better serve as graph axes
 species_options = ['Adelie', 'Gentoo', 'Chinstrap']
 selected_species = st.selectbox('Choose an option:', options=species_options)
 filtered_df = df[df['species'] == selected_species]
@@ -66,7 +65,7 @@ st.scatter_chart(
     color = 'Sex'
 )
 
-# Shows average body mass by species to show size differences
+# Shows average body mass by species to show size differences; uses seaborn for box plot functionality 
 st.title("Average Body Mass by Species")
 boxplot = sns.boxplot(
     x = "Island",
@@ -88,6 +87,7 @@ st.bar_chart(
 st.write(f'You selected: {selected_species}')
 
 # Island vs Body Mass; creates dictionary to rename variables to avoid renaming axis later on
+# Uses separate filtration system for part 2 of app to avoid confusion 
 island_options = ['Biscoe', 'Dream', 'Torgersen']
 selected_island = st.selectbox('Choose an option:', options=island_options)
 filtered3_df = df[df['island'] == selected_island]
@@ -112,7 +112,7 @@ col4.metric("Female %", round((filtered4_df["Sex"] == "female").mean()*100))
 col5.metric("Male %", round((filtered4_df["Sex"] == "male").mean()*100))
 
 # Analyzes Body Mass vs. Flipper Length; Island is individually selectable 
-st.title("Flipper Length vs. Body Mass (g) ")
+st.title("Flipper Length vs. Body Mass (g)")
 st.scatter_chart(
     data = filtered4_df,
     x = "Flipper Length (mm)",
