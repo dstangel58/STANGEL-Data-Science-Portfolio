f7f254e1ce6aca32882e4192c56b62af13359b79
Duncan Stangel <dstangel@nd.edu>
2026-04-14 11:00:12 -0400

    Change vote_map values from floats to integers

--- sml_streamlit_app/app.py
+++ sml_streamlit_app/app.py
@@ -52,8 +52,8 @@ with tab2: # the model itself; decision tree used to organize categorical data
     from sklearn.metrics import mean_squared_error, root_mean_squared_error, r2_score
 
     vote_map = {
-        'democrat' : 0.0,
-        'republican' : 1.0
+        'democrat' : 0,
+        'republican' : 1
         } # converts strings to floats 
     feature_map = {
         'y' : 1.0,
